6be6e74394c56f8422f6a5bcc530d2b9f9cb1b3c
toastercosplay <andrewdouglas2005@gmail.com>
2025-11-14 18:38:23 -0800

    setup
    blargh

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] public float chance = 0;
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
